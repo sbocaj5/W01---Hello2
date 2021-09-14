0eb95da069dacef9edc212fd8cbdfe9472f1e2d1
Alex Jacobs <alexjacobs295@gmail.com>
2021-09-14 10:04:41 -0600

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+String = "Hello World!"
+print(String)
+print(len(String))
+##To make a virtual machince type: py -m venv "folder name"
+## to ignore specific file make a .gitignore and type the files you want
+## 3 steps 
+# Stage - git add,
+# commit -git commit -m
+# synch git 
---
+++ README.md
@@ -0,0 +1,8 @@
+# Hello World Demo
+
+this shows how to say hello in python
+## Tools
+
+* python 
+* Visual Studio Code
+* Git / Github
